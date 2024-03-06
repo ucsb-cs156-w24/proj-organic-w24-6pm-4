ce4146d377e3115f803780b6899481f3d3d0f9ad
Bao Tran <tran.bao.n112@gmail.com>
2024-03-05 23:16:22 -0800

    bt - fixed error with edit page stories

--- frontend/src/stories/pages/Schools/SchoolsEditPage.stories.js
+++ frontend/src/stories/pages/Schools/SchoolsEditPage.stories.js
@@ -22,10 +22,10 @@ Default.parameters = {
         rest.get('/api/systemInfo', (_req, res, ctx) => {
             return res(ctx.json(systemInfoFixtures.showingNeither));
         }),
-        rest.get('/api/restaurants', (_req, res, ctx) => {
+        rest.get('/api/Schools', (_req, res, ctx) => {
             return res(ctx.json(SchoolsFixtures.threeSchools[0]));
         }),
-        rest.put('/api/restaurants', async (req, res, ctx) => {
+        rest.put('/api/Schools', async (req, res, ctx) => {
             var reqBody = await req.text();
             window.alert("PUT: " + req.url + " and body: " + reqBody);
             return res(ctx.status(200),ctx.json({}));
