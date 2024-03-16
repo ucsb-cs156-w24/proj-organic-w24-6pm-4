5c48ba82adefa4dfd08a56291f534a42d1a8f069
Bao Tran <tran.bao.n112@gmail.com>
2024-03-16 09:33:42 -0700

    Revert "bt - Update CourseForm.test.js"
    This reverts commit cb90962105a33a2c9d4767ec5ad91ab3495a4820.

--- frontend/src/tests/components/Courses/CourseForm.test.js
+++ frontend/src/tests/components/Courses/CourseForm.test.js
@@ -1,65 +1,78 @@
 import { render, waitFor, fireEvent, screen } from "@testing-library/react";
-import CoursesForm from "main/components/Courses/CoursesForm";
-import { coursesFixtures } from "fixtures/coursesFixtures";
+import CourseForm from "main/components/Courses/CourseForm";
+import { courseFixtures } from "fixtures/courseFixtures";
 import { BrowserRouter as Router } from "react-router-dom";
+
 import { QueryClient, QueryClientProvider } from "react-query";
 import { SchoolsFixtures } from "fixtures/SchoolsFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
+
 const mockedNavigate = jest.fn();
+
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useNavigate: () => mockedNavigate
 }));
-describe("CoursesForm tests", () => {
+
+
+describe("CourseForm tests", () => {
+    
     const axiosMock = new AxiosMockAdapter(axios);
+
     const setup = () => {
         axiosMock.reset();
         axiosMock.resetHistory();
+
+        axiosMock.onGet("/api/Schools/all").reply(200, SchoolsFixtures.threeSchools);
     }
+
     const queryClient = new QueryClient();
+    
     test("renders correctly", async () => {
-        render(
+
+       render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <CoursesForm />
+                    <CourseForm />
                 </Router>
             </QueryClientProvider>
         );
         await screen.findByText(/Name/);
         await screen.findByText(/Create/);
     });
+
+
     test("renders correctly when passing in a Courses", async () => {
-        setup();
-        axiosMock.onGet("/api/Schools/all").reply(200, SchoolsFixtures.threeSchools);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <CoursesForm initialContents={coursesFixtures.oneCourse}/>
+                    <CourseForm initialContents={courseFixtures.oneCourse}/>
                 </Router>
             </QueryClientProvider>
         );
-        await screen.findByTestId(/CoursesForm-id/);
-        
-        expect(screen.getByTestId("CoursesForm-school")).toHaveValue("ucsb");
-        
-        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+
+        await screen.findByTestId(/CourseForm-id/);
         expect(screen.getByText(/Id/)).toBeInTheDocument();
-        expect(screen.getByTestId(/CoursesForm-id/)).toHaveValue("1");
-        expect(screen.getByTestId("FormSelect-option-ucsb")).toBeInTheDocument();
+        expect(screen.getByTestId(/CourseForm-id/)).toHaveValue("1");
     });
+
+
     test("Correct Error messsages on missing input", async () => {
-        setup();
+        
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <CoursesForm />
+                    <CourseForm />
                 </Router>
             </QueryClientProvider>
         );
-        await screen.findByTestId("CoursesForm-submit");
-        const submitButton = screen.getByTestId("CoursesForm-submit");
+        await screen.findByTestId("CourseForm-submit");
+        const submitButton = screen.getByTestId("CourseForm-submit");
+
         fireEvent.click(submitButton);
+
         await screen.findByText(/Name is required./);
         expect(screen.getByText(/Name is required/)).toBeInTheDocument();
         expect(screen.getByText(/School is required./)).toBeInTheDocument();
@@ -68,55 +81,61 @@ describe("CoursesForm tests", () => {
         expect(screen.getByText(/EndDate date is required./)).toBeInTheDocument();
         expect(screen.getByText(/GithubOrg is required./)).toBeInTheDocument();
     });
+
     test("No Error messsages on good input", async () => {
+
         const mockSubmitAction = jest.fn();
+
+
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <CoursesForm submitAction={mockSubmitAction}/>
+                    <CourseForm submitAction={mockSubmitAction}/>
                 </Router>
             </QueryClientProvider>
         );
-        await screen.findByTestId("CoursesForm-name");
-        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+        await screen.findByTestId("CourseForm-name");
 
-        const nameField = screen.getByTestId("CoursesForm-name");
-        const schoolField = screen.getByTestId("CoursesForm-school");
-        const schoolSelect = screen.getByTestId("FormSelect");
-        const termField = screen.getByTestId("CoursesForm-term");
-        const startDateField = screen.getByTestId("CoursesForm-startDate");
-        const endDateField = screen.getByTestId("CoursesForm-endDate");
-        const githubOrgField = screen.getByTestId("CoursesForm-githubOrg")
-        const submitButton = screen.getByTestId("CoursesForm-submit");
+        const nameField = screen.getByTestId("CourseForm-name");
+        const schoolField = screen.getByTestId("CourseForm-school");
+        const termField = screen.getByTestId("CourseForm-term");
+        const startDateField = screen.getByTestId("CourseForm-startDate");
+        const endDateField = screen.getByTestId("CourseForm-endDate");
+        const githubOrgField = screen.getByTestId("CourseForm-githubOrg")
+        const submitButton = screen.getByTestId("CourseForm-submit");
 
         fireEvent.change(nameField, { target: { value: "CMPSC 156" } });
-        fireEvent.change(schoolSelect, {target : { value : 'ucsb'}});
+        fireEvent.change(schoolField, { target: { value: 'ucsb' } });
         fireEvent.change(termField, { target: { value: 'f23' } });
         fireEvent.change(startDateField, { target: { value: '2022-01-02T12:00' } });
         fireEvent.change(endDateField, { target: { value: '2022-02-02T12:00' } });
         fireEvent.change(githubOrgField, { target: { value: 'cs156-f23'}})
-
-        expect(schoolField).toHaveValue("ucsb");
-
         fireEvent.click(submitButton);
 
         await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
 
-        // expect(screen.getByText(/School is required./)).not.toBeInTheDocument();
         expect(screen.queryByText(/StartDate date is required./)).not.toBeInTheDocument();
         expect(screen.queryByText(/EndDate date is required./)).not.toBeInTheDocument();
+
     });
+
+
     test("that navigate(-1) is called when Cancel is clicked", async () => {
+
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <CoursesForm />
+                    <CourseForm />
                 </Router>
             </QueryClientProvider>
         );
-        await screen.findByTestId("CoursesForm-cancel");
-        const cancelButton = screen.getByTestId("CoursesForm-cancel");
+        await screen.findByTestId("CourseForm-cancel");
+        const cancelButton = screen.getByTestId("CourseForm-cancel");
+
         fireEvent.click(cancelButton);
+
         await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+
     });
+
 });
